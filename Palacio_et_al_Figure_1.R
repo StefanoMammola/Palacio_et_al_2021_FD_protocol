b26431184ff95d5d290e45f0aa40189e39bb8798
Stefano Mammola <68010284+StefanoMammola@users.noreply.github.com>
2021-06-17 18:00:20 +0300

    Add files via upload

--- Palacio_et_al_Figure_1.R
+++ Palacio_et_al_Figure_1.R
@@ -11,15 +11,22 @@
 
 # Loading R package -------------------------------------------------------
 
-library("dplyr")   # A Grammar of Data Manipulation
-library("ggplot2") # Create Elegant Data Visualisations Using the Grammar of Graphics
-library("wosr")    # Clients to the 'Web of Science' and 'InCites' APIs
+library("dplyr")     # A Grammar of Data Manipulation
+library("ggplot2")   # Create Elegant Data Visualisations Using the Grammar of Graphics
+library("gridExtra")
+library("wosr")      # Clients to the 'Web of Science' and 'InCites' APIs
+
+# Set working directory ---------------------------------------------------
+
+setwd("") #change me
 
 # Loading data on number of pubblication  ---------------------------------
 
 #Dimension database
-db_numbers  <- read.table(file = "/Users/stefanomammola/Desktop/WARNING ACCESSIBILITY/Database to provide/db_numbers.txt",header = TRUE)
-db_numbers <- db_numbers[db_numbers$PY>1999 & db_numbers$PY<2021,]
+db_numbers  <- read.csv(file = "/Users/stefanomammola/Desktop/Facundo FD protocol/dimension.csv",
+                        sep = ";", header = TRUE)
+
+db_numbers <- db_numbers[db_numbers$year > 1989 & db_numbers$year < 2021,] #selecting the range of year
 
 # WoS queries -------------------------------------------------------------
 
@@ -33,12 +40,13 @@ sid <- auth(NULL, password = NULL) #change with your WoS access
 coll <-  c("SCI", "SSCI", "AHCI", "ISTP", "ISSHP","BSCI", "BHCI", "ESCI")
 
 # Extracting N° publication by year ---------------------------------------
-range_year <- c(2000:2020)
+range_year <- c(1990:2020)
 
 functional_diversity   <- c()
 taxonomic_diversity    <- c()
 phylogenetic_diversity <- c()
 
+#Extracting WoS data 
 for (i in 1:length(range_year))  {
   
   query_1 <- query_wos(paste("TS = (\"functional diversity\") AND PY = (",range_year[i],")",sep=''), editions = coll, sid = sid)
@@ -53,7 +61,7 @@ for (i in 1:length(range_year))  {
 
 #Storing the result
 db <- data.frame(year = rep(range_year,3),
-                  Tot = rep(db_numbers$N,3),
+                  Tot = rep(db_numbers$publications,3),
                   N = c(functional_diversity,taxonomic_diversity,phylogenetic_diversity),
                   term = c(rep("functional diversity",length(range_year)),
                            rep("taxonomic_diversity",length(range_year)),
@@ -62,28 +70,78 @@ db <- data.frame(year = rep(range_year,3),
 # Plotting ----------------------------------------------------------------
 
 (p1 <- ggplot(db) +
+   
+   geom_line(aes(x=year, y=N, color=term),size=1.5,linetype = 1) + 
+   
+   scale_color_manual(values=c("orange", "black", "grey70"),
+                      labels = c( paste("Functional diversity [n= ",sum(functional_diversity),"]", sep=''),
+                                  paste("Phylogenetic diversity [n= ",sum(phylogenetic_diversity),"]", sep=''),
+                                  paste("Taxonomic diversity [n= ",sum(taxonomic_diversity),"]", sep='')))+
+   labs(x = NULL, 
+        y = "Number of published papers",
+        title = "A",
+        subtitle =  "Absolute values")+
+   
+   scale_x_continuous(breaks = c(seq(from=min(range_year),to=max(range_year),by=5)))+ 
+   
+   scale_y_continuous(breaks = c(0,500,1000,1500,2000,2500),
+                      labels = c("0","500","1000","1500","2000","2500"))+
+   
+   theme_bw()+
+   
+   theme(
+     legend.position = c(0.4, 0.7),
+     legend.title = element_blank(),
+     legend.text = element_text(size=10),
+     plot.title = element_text(color="black", size=14, face="bold"),
+     axis.title = element_text(size = 12),
+     axis.text.x = element_text(size = 11),
+     axis.text.y = element_text(size = 11),
+     panel.grid = element_blank(),
+     plot.caption = element_text(size = 10, color = "gray30")))
+
+(p2 <- ggplot(db) +
     
     geom_line(aes(x=year, y=N/Tot, color=term),size=1.5,linetype = 1) + 
     
-    scale_color_manual(values=c("orange","black","blue"),
+    scale_color_manual(values=c("orange", "black", "grey70"),
                        labels = c( paste("Functional diversity [n= ",sum(functional_diversity),"]", sep=''),
                                    paste("Taxonomic diversity [n= ",sum(taxonomic_diversity),"]", sep=''),
-                                   paste("Phylogenetic diversity [n= ",sum(phylogenetic_diversity),"]", sep=''))))+
-    labs(x = NULL, 
-         y = "Relative number of published papers in the Web of Science",
-         caption = NULL)+
-    
+                                   paste("Phylogenetic diversity [n= ",sum(phylogenetic_diversity),"]", sep='')))+
+  labs(x = NULL, 
+       title = "B",
+       subtitle = paste( "Relative to the annual number of published papers"),
+       y = "Relative number of published papers")+
+  
     scale_x_continuous(breaks = c(seq(from=min(range_year),to=max(range_year),by=5)))+ 
     
-    theme_bw()+
-    
-    theme(
-      legend.position = c(0.4, 0.7),
+    theme_bw()+theme(
+      legend.position = 'none',
       legend.title = element_blank(),
       legend.text = element_text(size=10),
+      plot.title = element_text(color="black", size=14, face="bold"),
       axis.title = element_text(size = 12),
       axis.text.x = element_text(size = 11),
       axis.text.y = element_text(size = 11),
       panel.grid = element_blank(),
-      plot.caption = element_text(size = 10, color = "gray30")
+      plot.caption = element_text(size = 10, color = "gray30"))
 )
+
+
+
+plots <- list(p1,p2) ; grobs <- list() ; widths <- list()
+
+for (i in 1:length(plots)){
+  grobs[[i]] <- ggplotGrob(plots[[i]])
+  widths[[i]] <- grobs[[i]]$widths[2:5]
+}
+
+maxwidth <- do.call(grid::unit.pmax, widths)
+
+for (i in 1:length(grobs)){
+  grobs[[i]]$widths[2:5] <- as.list(maxwidth)
+}
+
+pdf("/Users/stefanomammola/Desktop/Figure_1.pdf", width = 10, height = 4)
+do.call("grid.arrange", c(grobs, nrow = 1, ncol = 2))
+dev.off()
